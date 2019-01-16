85bd7c6d7a583113afb0fb0e3e4f201d1f4e6033
Pius Wong <pius@pioslabs.com>
2019-01-16 15:25:20 -0600

    Read in more complex DB info as JSON; new models, controllers

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -10,8 +10,13 @@ class ProjectsController extends Controller
     {
         // get model
         // $titles = ['temp1','temp2','temp3']; // debug code
-        $projects = \App\Project::all();
+        // $projects = \App\Project::all();
+        $projects = \App\Episode::all();
         // return $projects; // default returns as JSON format
+        $guests = \App\Guest::all();
+            // create a new model for each table? i.e. in PowerShell:
+            // php artisan make:model Guest
+            // creates Guest.php in app/http/
 
         $titles = $projects->map->title; // created w/ php artisan make:model Project
             // PS4 namespace structure
@@ -21,6 +26,7 @@ class ProjectsController extends Controller
         // return view('projects.titles',[ // searches 'projects' directory in 'views'
         //     'titles' => $titles
         // ]);
-        return view('projects.titles', compact('projects','titles')); // shortened code
+        // return view('projects.titles', compact('projects','titles')); // shortened code
+        return view('projects.titles', compact('projects','titles','guests')); // shortened code
     }
 }
